35bcc3c796cef18ac549881a16fe34f76d3bc428
trishitadhara <trishitadhara123@gmail.com>
2023-06-27 14:29:51 -0400

    flask app and predict pipeline added

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -16,17 +16,14 @@ from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path=os.path.join('artifacts',"proprocessor.pkl")
+    preprocessor_obj_file_path=os.path.join('artifacts',"preprocessor.pkl")
 
 class DataTransformation:
     def __init__(self):
         self.data_transformation_config=DataTransformationConfig()
 
     def get_data_transformer_object(self):
-        '''
-        This function si responsible for data trnasformation
-        
-        '''
+   
         try:
             numerical_columns = ["writing_score", "reading_score"]
             categorical_columns = [
